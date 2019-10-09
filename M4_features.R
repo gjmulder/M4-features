11c428ce0c6954fa55944636e88a070bb80b4e14
Gary Mulder <gjmulder@gmail.com>
2019-10-09 14:48:34 +0300

    Scatter plots of M4 forecast MASEs vs sMAPEs

--- M4_features.R
+++ M4_features.R
@@ -18,11 +18,11 @@ if (interactive()) {
 {
   prop_ts <- NA
 }
-use_parallel <- is.na(prop_ts)
+use_parallel <- TRUE #is.na(prop_ts)
 m4_freqs <- read_csv("m4_horiz.csv")
 horizons <- as.list(m4_freqs$Horizon)
 names(horizons) <- m4_freqs$SP
-err_names <- c("sMAPE", "MASE")
+err_names <- c("sMAPE", "MASE", "OWA")
 
 ###########################################################################
 # Preprocess M4 data ####
@@ -30,7 +30,9 @@ err_names <- c("sMAPE", "MASE")
 if (is.na(prop_ts)) {
   m4_data <- M4
 } else {
-  m4_data <- sample(M4, prop_ts * length(M4))
+  m4_data <- Filter(function(ts)
+    ts$period == "Monthly", M4)
+  # m4_data <- sample(M4, prop_ts * length(M4))
 }
 
 m4_data_x <-
@@ -107,186 +109,210 @@ fcast_smapes_df <-
   bind_rows(lapply(fcast_errs,
                    function(errs)
                      return(errs[1, ])))
-colnames(fcast_smapes_df) <-
-  paste0(colnames(fcast_smapes_df), "_smape")
+# colnames(fcast_smapes_df) <-
+#   paste0(colnames(fcast_smapes_df), "_smape")
 
 fcast_mases_df <-
   bind_rows(lapply(fcast_errs,
                    function(errs)
                      return(errs[2, ])))
-colnames(fcast_mases_df) <-
-  paste0(colnames(fcast_mases_df), "_mase")
+# colnames(fcast_mases_df) <-
+#   paste0(colnames(fcast_mases_df), "_mase")
 
 ###########################################################################
-# Features ####
+# Compute error summaries ####
 
-m4_data_proc <- purrr::map(m4_data,
-                           function(x) {
-                             tspx <- tsp(x$x)
-                             ts(c(x$x, x$xx), start = tspx[1], frequency = tspx[3])
-                           })
-khs_stl <- function(x, ...) {
-  lambda <- BoxCox.lambda(x,
-                          lower = 0,
-                          upper = 1,
-                          method = 'loglik')
-  y <- BoxCox(x, lambda)
-  c(stl_features(y, s.window = 'periodic', robust = TRUE, ...),
-    lambda = lambda)
-}
+mean_errs_df <-
+  as.data.frame(lapply(fcast_names, mean_fcast_errs, fcast_errs))
+colnames(mean_errs_df) <- fcast_names
+mean_errs_df <-
+  rbind(mean_errs_df,
+        colMeans(mean_errs_df / mean_errs_df$naive2))
+rownames(mean_errs_df) <- err_names
+print(round(mean_errs_df, 3))
+# write_csv(as.data.frame(t(mean_errs_df)), path = paste0("benchmark_m4_", m4_season, ".csv"))
 
-khs <-
-  bind_cols(
-    tsfeatures(m4_data_proc, c("frequency", "entropy"), parallel = use_parallel),
-    tsfeatures(
-      m4_data_proc,
-      "khs_stl",
-      scale = FALSE,
-      parallel = use_parallel
-    )
-  ) %>%
-  select(frequency, entropy, trend, seasonal_strength, e_acf1, lambda) %>%
-  replace_na(list(seasonal_strength = 0)) %>%
-  rename(
-    Frequency = frequency,
-    Entropy = entropy,
-    Trend = trend,
-    Season = seasonal_strength,
-    ACF1 = e_acf1,
-    Lambda = lambda
-  ) %>%
-  mutate(Period = as.factor(Frequency))
-
-# khs %>%
-#   select(Period, Entropy, Trend, Season, ACF1, Lambda) %>%
-#   GGally::ggpairs()
-
-###########################################################################
-# Compute correlation matrix ####
-
-m4_all_df <-
-  bind_cols(
-    khs %>%
-      select(Period, Entropy, Trend, Season, ACF1, Lambda),
-    fcast_smapes_df %>%
-      select(
-        naive2_smape,
-        ses_smape,
-        holt_smape,
-        holt_damped_smape,
-        theta_classic_smape,
-        combined_smape
-      ),
-    fcast_mases_df %>%
-      select(
-        naive2_mase,
-        ses_mase,
-        holt_mase,
-        holt_damped_mase,
-        theta_classic_mase,
-        combined_mase
-      )
-  )
-
-# m4_all_df[is.na(m4_all_df)] <- 0.0
-m4_all_df$Period <- as.numeric(as.character(m4_all_df$Period))
-cor_base_mtx <- round(cor(m4_all_df), 2)
-cor_base_mtx[lower.tri(cor_base_mtx)] <- NA
-
-cor_tri_df <- as.data.frame(cor_base_mtx) %>%
-  mutate(Var1 = factor(row.names(.), levels = row.names(.))) %>%
-  gather(
-    key = Var2,
-    value = value,
-    -Var1,
-    na.rm = TRUE,
-    factor_key = TRUE
-  )
-
-gg <- ggplot(cor_tri_df, aes(Var2, Var1, fill = value)) +
-  ggtitle(
-    paste0(
-      "Feature cross correlation for different statistical methods. ",
-      nrow(m4_all_df),
-      " time series."
-    )
-  ) +
-  geom_tile(color = "white") +
-  scale_fill_gradient2(
-    low = "blue",
-    high = "red",
-    mid = "white",
-    midpoint = 0,
-    limit = c(-1, 1),
-    space = "Lab",
-    name = "Correlation"
-  ) +
-  theme_minimal() + # minimal theme
-  theme(axis.text.x = element_text(
-    angle = 45,
-    vjust = 1,
-    size = 12,
-    hjust = 1
-  )) +
-  theme(axis.text.y = element_text(
-    vjust = 1,
-    size = 12,
-    hjust = 1
-  )) +
-  coord_fixed() +
-  geom_text(aes(Var2, Var1, label = value),
-            color = "black",
-            size = 4) +
-  theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank(),
-    axis.ticks = element_blank(),
-    legend.justification = c(1, 0),
-    legend.position = c(0.6, 0.7),
-    legend.direction = "horizontal"
-  ) +
-  guides(fill = guide_colorbar(
-    barwidth = 7,
-    barheight = 1,
-    title.position = "top",
-    title.hjust = 0.5
-  ))
-
-print(gg)
-if (!interactive()) {
-  ggsave(
-    "correlation_mtx.png",
-    dpi = 100,
-    scale = 5,
-    width = 2,
-    height = 2,
-    units = "in"
-  )
-}
-
-colnames(m4_all_df) <- sub("_", "\n", sub("_", "\n", colnames(m4_all_df)))
-gg <- GGally::ggpairs(m4_all_df,
-                      # upper = list(colour = "black"),
-                      lower = list(
-                        continuous = wrap("points", size = 0.2),
-                        combo = wrap("dot", size = 0.2)
-                      )) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size=0.1))
+fcast <- "combined"
+gg <-
+  ggplot(data = tibble(smape = fcast_smapes_df[[fcast]], mase = fcast_mases_df[[fcast]])) +
+  geom_point(aes(x = smape, y = mase), size = 0.2) +
+  scale_y_log10()+
+  scale_x_log10()+
+  geom_smooth(aes(x = smape, y = mase), method = "lm") +
+  geom_hline(yintercept = 1.0, size = 1) +
+  ggtitle(fcast)
 print(gg)
 
-if (!interactive()) {
-  ggsave(
-    "ggallyn.png",
-    dpi = 100,
-    scale = 7,
-    width = 2,
-    height = 2,
-    units = "in"
-  )
-}
+# ###########################################################################
+# # Features ####
+#
+# m4_data_proc <- purrr::map(m4_data,
+#                            function(x) {
+#                              tspx <- tsp(x$x)
+#                              ts(c(x$x, x$xx), start = tspx[1], frequency = tspx[3])
+#                            })
+# khs_stl <- function(x, ...) {
+#   lambda <- BoxCox.lambda(x,
+#                           lower = 0,
+#                           upper = 1,
+#                           method = 'loglik')
+#   y <- BoxCox(x, lambda)
+#   c(stl_features(y, s.window = 'periodic', robust = TRUE, ...),
+#     lambda = lambda)
+# }
+#
+# khs <-
+#   bind_cols(
+#     tsfeatures(m4_data_proc, c("frequency", "entropy"), parallel = use_parallel),
+#     tsfeatures(
+#       m4_data_proc,
+#       "khs_stl",
+#       scale = FALSE,
+#       parallel = use_parallel
+#     )
+#   ) %>%
+#   select(frequency, entropy, trend, seasonal_strength, e_acf1, lambda) %>%
+#   replace_na(list(seasonal_strength = 0)) %>%
+#   rename(
+#     Frequency = frequency,
+#     Entropy = entropy,
+#     Trend = trend,
+#     Season = seasonal_strength,
+#     ACF1 = e_acf1,
+#     Lambda = lambda
+#   ) %>%
+#   mutate(Period = as.factor(Frequency))
+#
+# # khs %>%
+# #   select(Period, Entropy, Trend, Season, ACF1, Lambda) %>%
+# #   GGally::ggpairs()
+#
+# ###########################################################################
+# # Compute correlation matrix ####
+#
+# m4_all_df <-
+#   bind_cols(
+#     khs %>%
+#       select(Period, Entropy, Trend, Season, ACF1, Lambda),
+#     fcast_smapes_df %>%
+#       select(
+#         naive2_smape,
+#         ses_smape,
+#         holt_smape,
+#         holt_damped_smape,
+#         theta_classic_smape,
+#         combined_smape
+#       ),
+#     fcast_mases_df %>%
+#       select(
+#         naive2_mase,
+#         ses_mase,
+#         holt_mase,
+#         holt_damped_mase,
+#         theta_classic_mase,
+#         combined_mase
+#       )
+#   )
+#
+# # m4_all_df[is.na(m4_all_df)] <- 0.0
+# m4_all_df$Period <- as.numeric(as.character(m4_all_df$Period))
+# cor_base_mtx <- round(cor(m4_all_df), 2)
+# cor_base_mtx[lower.tri(cor_base_mtx)] <- NA
+#
+# cor_tri_df <- as.data.frame(cor_base_mtx) %>%
+#   mutate(Var1 = factor(row.names(.), levels = row.names(.))) %>%
+#   gather(
+#     key = Var2,
+#     value = value,
+#     -Var1,
+#     na.rm = TRUE,
+#     factor_key = TRUE
+#   )
+#
+# gg <- ggplot(cor_tri_df, aes(Var2, Var1, fill = value)) +
+#   ggtitle(
+#     paste0(
+#       "Feature cross correlation for different statistical methods. ",
+#       nrow(m4_all_df),
+#       " time series."
+#     )
+#   ) +
+#   geom_tile(color = "white") +
+#   scale_fill_gradient2(
+#     low = "blue",
+#     high = "red",
+#     mid = "white",
+#     midpoint = 0,
+#     limit = c(-1, 1),
+#     space = "Lab",
+#     name = "Correlation"
+#   ) +
+#   theme_minimal() + # minimal theme
+#   theme(axis.text.x = element_text(
+#     angle = 45,
+#     vjust = 1,
+#     size = 12,
+#     hjust = 1
+#   )) +
+#   theme(axis.text.y = element_text(
+#     vjust = 1,
+#     size = 12,
+#     hjust = 1
+#   )) +
+#   coord_fixed() +
+#   geom_text(aes(Var2, Var1, label = value),
+#             color = "black",
+#             size = 4) +
+#   theme(
+#     axis.title.x = element_blank(),
+#     axis.title.y = element_blank(),
+#     panel.grid.major = element_blank(),
+#     panel.border = element_blank(),
+#     panel.background = element_blank(),
+#     axis.ticks = element_blank(),
+#     legend.justification = c(1, 0),
+#     legend.position = c(0.6, 0.7),
+#     legend.direction = "horizontal"
+#   ) +
+#   guides(fill = guide_colorbar(
+#     barwidth = 7,
+#     barheight = 1,
+#     title.position = "top",
+#     title.hjust = 0.5
+#   ))
+#
+# print(gg)
+# if (!interactive()) {
+#   ggsave(
+#     "correlation_mtx.png",
+#     dpi = 100,
+#     scale = 5,
+#     width = 2,
+#     height = 2,
+#     units = "in"
+#   )
+# }
+#
+# colnames(m4_all_df) <- sub("_", "\n", sub("_", "\n", colnames(m4_all_df)))
+# gg <- GGally::ggpairs(m4_all_df,
+#                       # upper = list(colour = "black"),
+#                       lower = list(
+#                         continuous = wrap("points", size = 0.2),
+#                         combo = wrap("dot", size = 0.2)
+#                       )) +
+#   theme(axis.text.x = element_text(angle = 45, hjust = 1, size=0.1))
+# print(gg)
+#
+# if (!interactive()) {
+#   ggsave(
+#     "ggallyn.png",
+#     dpi = 100,
+#     scale = 7,
+#     width = 2,
+#     height = 2,
+#     units = "in"
+#   )
+# }
 
 # fit_lm <- function(method_err_name){
 #   formula <- as.formula(paste(method_err_name, "~", paste(colnames(m4_feat_df)[1:(ncol(m4_feat_df)-2)], collapse='+')))
