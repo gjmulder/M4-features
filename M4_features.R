d627b1ac0e15ea6546199f0814bbd9ed994cf1df
Gary Mulder <gjmulder@gmail.com>
2019-10-10 15:45:50 +0300

    Working tabular percentage proportions of forecasts

--- M4_features.R
+++ M4_features.R
@@ -107,138 +107,77 @@ if (use_parallel) {
 }
 
 fcast_smapes_df <-
-  bind_rows(lapply(fcast_errs,
-                   function(errs)
-                     return(errs[1, ])))
-colnames(fcast_smapes_df) <-
-  paste0(colnames(fcast_smapes_df), "_smape")
+  unlist(lapply(fcast_errs,
+                function(errs)
+                  return(names(which.min(
+                    errs[1,]
+                  )))))
 
 fcast_mases_df <-
-  bind_rows(lapply(fcast_errs,
-                   function(errs)
-                     return(errs[2,])))
-colnames(fcast_mases_df) <-
-  paste0(colnames(fcast_mases_df), "_mase")
-
-###########################################################################
-# Features ####
-
-m4_data_proc <- purrr::map(m4_data,
-                           function(x) {
-                             tspx <- tsp(x$x)
-                             ts(c(x$x, x$xx), start = tspx[1], frequency = tspx[3])
-                           })
-khs_stl <- function(x, ...) {
-  lambda <- BoxCox.lambda(x,
-                          lower = 0,
-                          upper = 1,
-                          method = 'loglik')
-  y <- BoxCox(x, lambda)
-  c(stl_features(y, s.window = 'periodic', robust = TRUE, ...),
-    lambda = lambda)
-}
-
-khs <-
-  bind_cols(
-    tsfeatures(m4_data_proc, c("frequency", "entropy"), parallel = use_parallel),
-    tsfeatures(
-      m4_data_proc,
-      "khs_stl",
-      scale = FALSE,
-      parallel = use_parallel
-    )
-  ) %>%
-  select(frequency, entropy, trend, seasonal_strength, e_acf1, lambda) %>%
-  replace_na(list(seasonal_strength = 0)) %>%
-  rename(
-    Frequency = frequency,
-    Entropy = entropy,
-    Trend = trend,
-    Season = seasonal_strength,
-    ACF1 = e_acf1,
-    Lambda = lambda
-  ) #%>%
-#mutate(Period = as.factor(Frequency))
-
-###########################################################################
-# Combine forecasts and features ####
+  unlist(lapply(fcast_errs,
+                function(errs)
+                  return(names(which.min(
+                    errs[2,]
+                  )))))
 
 m4_data_all_df <-
-  bind_cols(khs, fcast_mases_df, fcast_smapes_df)
-m4_data_all_df$type <- unlist(m4_type)
-m4_data_all_df$period <- unlist(m4_period)
+  tibble(
+    best_mases = fcast_mases_df,
+    best_smapes = fcast_smapes_df,
+    type = as.character(unlist(m4_type)),
+    period = as.character(unlist(m4_period))
+  )
+
+prop_str <- function(x) {
+  pt <- round(100 * prop.table(table(x)))
+  return(paste0(names(pt), sprintf(":%4d%%", pt), "\n", collapse = ''))
+}
 
 m4_data_all_df %>%
   group_by(type, period) %>%
-  summarise_if(is.numeric, list(mean = mean, sd = sd), na.rm = TRUE) %>%
-  ungroup %>%
-  mutate(type = as.character(type)) %>%
-  mutate(period = as.character(period)) ->
+  summarise(data = prop_str(best_mases)) ->
   m4_type_period_df
 
 m4_data_all_df %>%
   group_by(type) %>%
-  summarise_if(is.numeric, list(mean = mean, sd = sd), na.rm = TRUE) %>%
-  ungroup %>%
-  mutate(type = as.character(type)) %>%
+  summarise(data = prop_str(best_mases)) %>%
   mutate(period = "Total") ->
   m4_type_df
 
 m4_data_all_df %>%
+  summarise(data = prop_str(best_mases)) %>%
+  mutate(period = "Total") %>%
+  mutate(type = "Total") ->
+  m4_total_df
+
+m4_data_all_df %>%
   group_by(period) %>%
-  summarise_if(is.numeric, list(mean = mean, sd = sd), na.rm = TRUE) %>%
-  ungroup %>%
-  mutate(period = as.character(period)) %>%
+  summarise(data = prop_str(best_mases)) %>%
   mutate(type = "Total") ->
   m4_period_df
 
-m4_data_all_df %>%
-  summarise_if(is.numeric, list(mean = mean, sd = sd), na.rm = TRUE) %>%
-  mutate(type = "Total") %>%
-  mutate(period = "Total") ->
-  totals_df
-
-bind_rows(m4_type_period_df, m4_type_df, m4_period_df, totals_df) %>%
-  mutate(
-    data = sprintf(
-      "%7.2f%7.2f\n%7.2f%7.2f\n%7.2f%7.2f\n%7.2f%7.2f\n%7.2f%7.2f\n%7.2f%7.2f\n%7.2f%7.2f",
-      # "%7.2f%7.2f\n%7.2f%7.2f",
-      Entropy_mean,
-      Entropy_sd,
-      Trend_mean,
-      Trend_sd,
-      Season_mean,
-      Season_sd,
-      ACF1_mean,
-      ACF1_sd,
-      Lambda_mean,
-      Lambda_sd,
-      combined_mase_mean,
-      combined_mase_sd,
-      combined_smape_mean,
-      combined_smape_sd
-    )
-  ) %>%
-  select(type, period, data) %>%
+bind_rows(m4_type_period_df, m4_type_df, m4_period_df, m4_total_df) %>%
   spread(type, data) %>%
-  select(Micro, Industry, Macro, Finance, Demographic, Other, Total, period) %>%
-  arrange(c(7, 1, 2, 3, 4, 5, 6)) ->
+  arrange(c(7, 4, 3, 6, 1, 2, 5)) ->
   results_df
 
-col_names <-  paste0(colnames(results_df), "\nmean   sd")
-col_names[length(col_names)] <- "Freq"
-
-# png('feat_freqs.png', width = 4096, height = 4096)
 dev.off()
-print(grid.table(
-  results_df[c(6, 3, 2, 5, 7, 1, 4),],
-  rows = rep(
-    "Entropy\nTrend\nSeason\nACF1\nLambda\nMASE\nsMAPE",
-    nrow(results_df)
-  ),
-  cols = col_names
-))
-# print(grid.table(results_df, rows = rep("Entropy\nMASE", nrow(results_df)), cols = col_names))
+print(grid.table(results_df))
+
+# ,
+# rows = rep(
+#   "Entropy\nTrend\nSeason\nACF1\nLambda\nMASE\nsMAPE",
+#   nrow(results_df)
+# ),
+# cols = col_names
+# # print(grid.table(results_df, rows = rep("Entropy\nMASE", nrow(results_df)), cols = col_names))
+# select(Micro, Industry, Macro, Finance, Demographic, Other, Total, period) %>%
+# arrange(c(7, 1, 2, 3, 4, 5, 6)) ->
+#
+# col_names <-  paste0(colnames(results_df), "\nmean   sd")
+# col_names[length(col_names)] <- "Freq"
+#
+# # png('feat_freqs.png', width = 4096, height = 4096)
 
 # ###########################################################################
 # # Compute correlation matrix ####
