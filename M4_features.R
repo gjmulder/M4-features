0ed3d2fed282f9f6bf33012cfc6a45f36b2d1b8f
Gary Mulder <gjmulder@gmail.com>
2019-10-11 14:36:22 +0300

    Smoke test processing Slawek's data

--- M4_features.R
+++ M4_features.R
@@ -40,15 +40,17 @@ m4_data_x <-
   lapply(m4_data, function(ts)
     return(ts$x))
 
+m4_st <-
+  lapply(m4_data, function(ts)
+    return(ts$st))
+
 m4_type <-
   lapply(m4_data, function(ts)
     return(ts$type))
-names(m4_type) <- "type"
 
 m4_period <-
   lapply(m4_data, function(ts)
     return(ts$period))
-names(m4_period) <- "period"
 
 m4_horiz <-
   lapply(1:length(m4_data_x), function(idx)
@@ -70,6 +72,8 @@ m4_data_xx <-
 
 print("M4 Competition data:")
 
+fcasts_slawek <- load_slawek_data(output_dir)
+
 if (use_parallel) {
   fcasts <- mclapply(
     1:length(m4_data_x),
@@ -88,89 +92,89 @@ if (use_parallel) {
 }
 fcast_names <- names(fcasts[[1]])
 
-###########################################################################
-# Compute sMAPE and MASE ####
-
-if (use_parallel) {
-  fcast_errs <- mclapply(1:length(fcasts),
-                         compute_fcast_errs,
-                         fcasts,
-                         m4_data_x,
-                         m4_data_xx,
-                         mc.cores = 16)
-} else {
-  fcast_errs <- lapply(1:length(fcasts),
-                       compute_fcast_errs,
-                       fcasts,
-                       m4_data_x,
-                       m4_data_xx)
-}
-
-fcast_smapes_df <-
-  unlist(lapply(fcast_errs,
-                function(errs)
-                  return(names(which.min(
-                    errs[1,]
-                  )))))
-
-fcast_mases_df <-
-  unlist(lapply(fcast_errs,
-                function(errs)
-                  return(names(which.min(
-                    errs[2,]
-                  )))))
-
-m4_data_all_df <-
-  tibble(
-    best_mases = fcast_mases_df,
-    best_smapes = fcast_smapes_df,
-    type = as.character(unlist(m4_type)),
-    period = as.character(unlist(m4_period))
-  )
-
-prop_str <- function(x) {
-  pt <- round(100 * prop.table(table(x)), 1)
-  return(paste0(names(pt), sprintf(":%5.1f%%", pt), "\n", collapse = ''))
-}
-
-###########################################################################
-# Generate percentage best method table for each period and type ####
-
-m4_data_all_df %>%
-  group_by(type, period) %>%
-  summarise(data = prop_str(best_mases)) ->
-  m4_type_period_df
-
-m4_data_all_df %>%
-  group_by(type) %>%
-  summarise(data = prop_str(best_mases)) %>%
-  mutate(period = "Total") ->
-  m4_type_df
-
-m4_data_all_df %>%
-  summarise(data = prop_str(best_mases)) %>%
-  mutate(period = "Total") %>%
-  mutate(type = "Total") ->
-  m4_total_df
-
-m4_data_all_df %>%
-  group_by(period) %>%
-  summarise(data = prop_str(best_mases)) %>%
-  mutate(type = "Total") ->
-  m4_period_df
-
-bind_rows(m4_type_period_df, m4_type_df, m4_period_df, m4_total_df) %>%
-  spread(type, data) %>%
-  select(Micro, Industry, Macro, Finance, Demographic, Other, Total, period) ->
-  results_df
-
-results_df <- as.data.frame(results_df)
-rownames(results_df) <- results_df$period
-results_df$period <- NULL
-
-tt <- gridExtra::ttheme_default(
-  core = list(fg_params=list(cex = 0.8)),
-  colhead = list(fg_params=list(cex = 0.8)),
-  rowhead = list(fg_params=list(cex = 0.8)))
-dev.off()
-print(grid.table(results_df[c(7, 4, 3, 6, 1, 2, 5),], theme=tt))
+# ###########################################################################
+# # Compute sMAPE and MASE ####
+#
+# if (use_parallel) {
+#   fcast_errs <- mclapply(1:length(fcasts),
+#                          compute_fcast_errs,
+#                          fcasts,
+#                          m4_data_x,
+#                          m4_data_xx,
+#                          mc.cores = 16)
+# } else {
+#   fcast_errs <- lapply(1:length(fcasts),
+#                        compute_fcast_errs,
+#                        fcasts,
+#                        m4_data_x,
+#                        m4_data_xx)
+# }
+#
+# fcast_smapes_df <-
+#   unlist(lapply(fcast_errs,
+#                 function(errs)
+#                   return(names(which.min(
+#                     errs[1,]
+#                   )))))
+#
+# fcast_mases_df <-
+#   unlist(lapply(fcast_errs,
+#                 function(errs)
+#                   return(names(which.min(
+#                     errs[2,]
+#                   )))))
+#
+# m4_data_all_df <-
+#   tibble(
+#     best_mases = fcast_mases_df,
+#     best_smapes = fcast_smapes_df,
+#     type = as.character(unlist(m4_type)),
+#     period = as.character(unlist(m4_period))
+#   )
+#
+# prop_str <- function(x) {
+#   pt <- round(100 * prop.table(table(x)), 1)
+#   return(paste0(names(pt), sprintf(":%5.1f%%", pt), "\n", collapse = ''))
+# }
+#
+# ###########################################################################
+# # Generate percentage best method table for each period and type ####
+#
+# m4_data_all_df %>%
+#   group_by(type, period) %>%
+#   summarise(data = prop_str(best_mases)) ->
+#   m4_type_period_df
+#
+# m4_data_all_df %>%
+#   group_by(type) %>%
+#   summarise(data = prop_str(best_mases)) %>%
+#   mutate(period = "Total") ->
+#   m4_type_df
+#
+# m4_data_all_df %>%
+#   summarise(data = prop_str(best_mases)) %>%
+#   mutate(period = "Total") %>%
+#   mutate(type = "Total") ->
+#   m4_total_df
+#
+# m4_data_all_df %>%
+#   group_by(period) %>%
+#   summarise(data = prop_str(best_mases)) %>%
+#   mutate(type = "Total") ->
+#   m4_period_df
+#
+# bind_rows(m4_type_period_df, m4_type_df, m4_period_df, m4_total_df) %>%
+#   spread(type, data) %>%
+#   select(Micro, Industry, Macro, Finance, Demographic, Other, Total, period) ->
+#   results_df
+#
+# results_df <- as.data.frame(results_df)
+# rownames(results_df) <- results_df$period
+# results_df$period <- NULL
+#
+# tt <- gridExtra::ttheme_default(
+#   core = list(fg_params=list(cex = 0.8)),
+#   colhead = list(fg_params=list(cex = 0.8)),
+#   rowhead = list(fg_params=list(cex = 0.8)))
+# dev.off()
+# print(grid.table(results_df[c(7, 4, 3, 6, 1, 2, 5),], theme=tt))
--- fcast.R
+++ fcast.R
@@ -1,7 +1,8 @@
 library(forecast)
 source("benchmarks_eval.R")
+library(tidyverse)
 
-multi_fit_ts <- function(idx, data_x, data_x_deseason, m4_data_x_horiz) {
+multi_fit_ts <- function(idx, fcasts_slawek, data_st, data_x, data_x_deseason, m4_data_x_horiz) {
   fcast_naive <- naive(data_x[[idx]], h = m4_data_x_horiz[[idx]])$mean
   fcast_seasonal_naive <-
     seasonal_naive(data_x[[idx]], fcast_horiz = m4_data_x_horiz[[idx]])
@@ -55,6 +56,21 @@ multi_fit_ts <- function(idx, data_x, data_x_deseason, m4_data_x_horiz) {
       combined       = (fcast_ses + fcast_holt + fcast_holt_damped) / 3
     )
   )
+}
+
+load_slawek_data <- function(output_dir) {
+  split_file <- function(fname) {
+    ts_split <- scan(fname, what = "", sep = "\n") %>%
+      lapply(str_split, ",")
+
+    fcasts <- lapply(ts_split, function(ts) return(as.numeric(ts[[1]][2:length(ts[[1]])])))
+    names(fcasts) <- unlist(lapply(ts_split, function(ts) return(ts[[1]][1])))
+    return(fcasts)
+  }
+  ll <- do.call(c, lapply(Sys.glob(
+    paste0(output_dir, "/*/*.csv")), split_file))
+  stopifnot(length(ll) == 28000L)
+  return(ll)
 }
 
 #######################################################################
