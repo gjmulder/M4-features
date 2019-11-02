9364aa86383db9c321a38cae93896924ceb2c15a
Gary Mulder <gjmulder@gmail.com>
2019-11-02 18:26:42 +0200

    Plots of different benchmarks through time

--- M4_features.R
+++ M4_features.R
@@ -1,8 +1,6 @@
 library(M4comp2018)
-library(tsfeatures)
+# library(tsfeatures)
 library(parallel)
-library(GGally)
-# library(ggplot2)
 library(tidyverse)
 
 set.seed(42)
@@ -13,15 +11,12 @@ source("fcast.R")
 # Config ####
 
 if (interactive()) {
-  prop_ts <- 0.01
+  prop_ts <- NA
 } else
 {
   prop_ts <- NA
 }
 use_parallel <- TRUE #is.na(prop_ts)
-m4_freqs <- read_csv("m4_horiz.csv")
-horizons <- as.list(m4_freqs$Horizon)
-names(horizons) <- m4_freqs$SP
 err_names <- c("sMAPE", "MASE", "OWA")
 
 ###########################################################################
@@ -39,23 +34,23 @@ m4_data_x <-
   lapply(m4_data, function(ts)
     return(ts$x))
 
-m4_type <-
-  lapply(m4_data, function(ts)
-    return(ts$type))
-names(m4_type) <- "type"
-
-m4_period <-
-  lapply(m4_data, function(ts)
-    return(ts$period))
-names(m4_period) <- "period"
+# m4_type <-
+#   lapply(m4_data, function(ts)
+#     return(ts$type))
+# names(m4_type) <- "type"
+#
+# m4_period <-
+#   lapply(m4_data, function(ts)
+#     return(ts$period))
+# names(m4_period) <- "period"
 
 m4_horiz <-
   lapply(1:length(m4_data_x), function(idx)
-    return(horizons[[as.character(m4_period[[idx]])]]))
+    return(m4_data[[idx]]$h))
 
 if (use_parallel) {
   m4_data_x_deseason <- mclapply(1:length(m4_data_x), function(idx)
-    return(deseasonalise(m4_data_x[[idx]], m4_horiz[[idx]])), mc.cores = 16)
+    return(deseasonalise(m4_data_x[[idx]], m4_horiz[[idx]])), mc.cores = 2)
 } else {
   m4_data_x_deseason <- lapply(1:length(m4_data_x), function(idx)
     return(deseasonalise(m4_data_x[[idx]], m4_horiz[[idx]])))
@@ -76,7 +71,7 @@ if (use_parallel) {
     m4_data_x,
     m4_data_x_deseason,
     m4_horiz,
-    mc.cores = 16
+    mc.cores = 2
   )
 } else {
   fcasts <- lapply(1:length(m4_data_x),
@@ -96,7 +91,7 @@ if (use_parallel) {
                          fcasts,
                          m4_data_x,
                          m4_data_xx,
-                         mc.cores = 16)
+                         mc.cores = 2)
 } else {
   fcast_errs <- lapply(1:length(fcasts),
                        compute_fcast_errs,
@@ -104,20 +99,6 @@ if (use_parallel) {
                        m4_data_x,
                        m4_data_xx)
 }
-
-fcast_smapes_df <-
-  bind_rows(lapply(fcast_errs,
-                   function(errs)
-                     return(errs[1, ])))
-# colnames(fcast_smapes_df) <-
-#   paste0(colnames(fcast_smapes_df), "_smape")
-
-fcast_mases_df <-
-  bind_rows(lapply(fcast_errs,
-                   function(errs)
-                     return(errs[2, ])))
-# colnames(fcast_mases_df) <-
-#   paste0(colnames(fcast_mases_df), "_mase")
 
 ###########################################################################
 # Compute error summaries ####
@@ -132,245 +113,9 @@ rownames(mean_errs_df) <- err_names
 print(round(mean_errs_df, 3))
 # write_csv(as.data.frame(t(mean_errs_df)), path = paste0("benchmark_m4_", m4_season, ".csv"))
 
-fcast <- "combined"
-gg <-
-  ggplot(data = tibble(smape = fcast_smapes_df[[fcast]], mase = fcast_mases_df[[fcast]])) +
-  geom_point(aes(x = smape, y = mase), size = 0.2) +
-  scale_y_log10()+
-  scale_x_log10()+
-  geom_smooth(aes(x = smape, y = mase), method = "lm") +
-  geom_hline(yintercept = 1.0, size = 1) +
-  ggtitle(fcast)
-print(gg)
-
-# ###########################################################################
-# # Features ####
-#
-# m4_data_proc <- purrr::map(m4_data,
-#                            function(x) {
-#                              tspx <- tsp(x$x)
-#                              ts(c(x$x, x$xx), start = tspx[1], frequency = tspx[3])
-#                            })
-# khs_stl <- function(x, ...) {
-#   lambda <- BoxCox.lambda(x,
-#                           lower = 0,
-#                           upper = 1,
-#                           method = 'loglik')
-#   y <- BoxCox(x, lambda)
-#   c(stl_features(y, s.window = 'periodic', robust = TRUE, ...),
-#     lambda = lambda)
-# }
-#
-# khs <-
-#   bind_cols(
-#     tsfeatures(m4_data_proc, c("frequency", "entropy"), parallel = use_parallel),
-#     tsfeatures(
-#       m4_data_proc,
-#       "khs_stl",
-#       scale = FALSE,
-#       parallel = use_parallel
-#     )
-#   ) %>%
-#   select(frequency, entropy, trend, seasonal_strength, e_acf1, lambda) %>%
-#   replace_na(list(seasonal_strength = 0)) %>%
-#   rename(
-#     Frequency = frequency,
-#     Entropy = entropy,
-#     Trend = trend,
-#     Season = seasonal_strength,
-#     ACF1 = e_acf1,
-#     Lambda = lambda
-#   ) %>%
-#   mutate(Period = as.factor(Frequency))
-#
-# # khs %>%
-# #   select(Period, Entropy, Trend, Season, ACF1, Lambda) %>%
-# #   GGally::ggpairs()
-#
-# ###########################################################################
-# # Compute correlation matrix ####
-#
-# m4_all_df <-
-#   bind_cols(
-#     khs %>%
-#       select(Period, Entropy, Trend, Season, ACF1, Lambda),
-#     fcast_smapes_df %>%
-#       select(
-#         naive2_smape,
-#         ses_smape,
-#         holt_smape,
-#         holt_damped_smape,
-#         theta_classic_smape,
-#         combined_smape
-#       ),
-#     fcast_mases_df %>%
-#       select(
-#         naive2_mase,
-#         ses_mase,
-#         holt_mase,
-#         holt_damped_mase,
-#         theta_classic_mase,
-#         combined_mase
-#       )
-#   )
-#
-# # m4_all_df[is.na(m4_all_df)] <- 0.0
-# m4_all_df$Period <- as.numeric(as.character(m4_all_df$Period))
-# cor_base_mtx <- round(cor(m4_all_df), 2)
-# cor_base_mtx[lower.tri(cor_base_mtx)] <- NA
-#
-# cor_tri_df <- as.data.frame(cor_base_mtx) %>%
-#   mutate(Var1 = factor(row.names(.), levels = row.names(.))) %>%
-#   gather(
-#     key = Var2,
-#     value = value,
-#     -Var1,
-#     na.rm = TRUE,
-#     factor_key = TRUE
-#   )
-#
-# gg <- ggplot(cor_tri_df, aes(Var2, Var1, fill = value)) +
-#   ggtitle(
-#     paste0(
-#       "Feature cross correlation for different statistical methods. ",
-#       nrow(m4_all_df),
-#       " time series."
-#     )
-#   ) +
-#   geom_tile(color = "white") +
-#   scale_fill_gradient2(
-#     low = "blue",
-#     high = "red",
-#     mid = "white",
-#     midpoint = 0,
-#     limit = c(-1, 1),
-#     space = "Lab",
-#     name = "Correlation"
-#   ) +
-#   theme_minimal() + # minimal theme
-#   theme(axis.text.x = element_text(
-#     angle = 45,
-#     vjust = 1,
-#     size = 12,
-#     hjust = 1
-#   )) +
-#   theme(axis.text.y = element_text(
-#     vjust = 1,
-#     size = 12,
-#     hjust = 1
-#   )) +
-#   coord_fixed() +
-#   geom_text(aes(Var2, Var1, label = value),
-#             color = "black",
-#             size = 4) +
-#   theme(
-#     axis.title.x = element_blank(),
-#     axis.title.y = element_blank(),
-#     panel.grid.major = element_blank(),
-#     panel.border = element_blank(),
-#     panel.background = element_blank(),
-#     axis.ticks = element_blank(),
-#     legend.justification = c(1, 0),
-#     legend.position = c(0.6, 0.7),
-#     legend.direction = "horizontal"
-#   ) +
-#   guides(fill = guide_colorbar(
-#     barwidth = 7,
-#     barheight = 1,
-#     title.position = "top",
-#     title.hjust = 0.5
-#   ))
-#
-# print(gg)
-# if (!interactive()) {
-#   ggsave(
-#     "correlation_mtx.png",
-#     dpi = 100,
-#     scale = 5,
-#     width = 2,
-#     height = 2,
-#     units = "in"
-#   )
-# }
-#
-# colnames(m4_all_df) <- sub("_", "\n", sub("_", "\n", colnames(m4_all_df)))
-# gg <- GGally::ggpairs(m4_all_df,
-#                       # upper = list(colour = "black"),
-#                       lower = list(
-#                         continuous = wrap("points", size = 0.2),
-#                         combo = wrap("dot", size = 0.2)
-#                       )) +
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1, size=0.1))
-# print(gg)
-#
-# if (!interactive()) {
-#   ggsave(
-#     "ggallyn.png",
-#     dpi = 100,
-#     scale = 7,
-#     width = 2,
-#     height = 2,
-#     units = "in"
-#   )
-# }
-
-# fit_lm <- function(method_err_name){
-#   formula <- as.formula(paste(method_err_name, "~", paste(colnames(m4_feat_df)[1:(ncol(m4_feat_df)-2)], collapse='+')))
-#   return(lm(formula, data = m4_all_df))
-# }
-# method_err_names <- c(colnames(fcast_smapes_df), colnames(fcast_mases_df))
-# if (use_parallel) {
-#   fits_lm <- mclapply(method_err_names, fit_lm, mc.cores = 16)
-# } else {
-#   fits_lm <- lapply(method_err_names, fit_lm)
-# }
-# names(fits_lm) <- method_err_names
-# print(lapply(fits_lm, summary))
-
-#   theme(
-#     axis.title.x = element_blank(),
-#     axis.title.y = element_blank(),
-#     panel.grid.major = element_blank(),
-#     panel.border = element_blank(),
-#     panel.background = element_blank(),
-#     axis.ticks = element_blank(),
-#     legend.justification = c(1, 0),
-#     legend.position = c(0.6, 0.7),
-#     legend.direction = "horizontal"
-#   ) +
-#   guides(fill = guide_colorbar(
-#     barwidth = 7,
-#     barheight = 1,
-#     title.position = "top",
-#     title.hjust = 0.5
-#   ))
-#
-# print(gg)
-# if (!interactive()) {
-#   # colnames(cor_base_mtx) <- sub("_", " ", sub("_", " ", colnames(cor_base_mtx)))
-#   # rownames(cor_base_mtx) <- sub("_", " ", sub("_", " ", rownames(cor_base_mtx)))
-#   # write.csv(as.data.frame(cor_base_mtx), "correlation_mtx.csv")
-#   # # cor_df <- as.data.frame(t(cor_base_mtx))
-#   # # write.csv(cor_df[rev(rownames(cor_df)),], "correlation_mtx.csv")
-#   ggsave(
-#     "correlation_mtx.png",
-#     dpi = 100,
-#     scale = 5,
-#     width = 2,
-#     height = 2,
-#     units = "in"
-#   )
-# }
-#
-# fit_lm <- function(method_err_name){
-#   formula <- as.formula(paste(method_err_name, "~", paste(colnames(m4_feat_df)[1:(ncol(m4_feat_df)-2)], collapse='+')))
-#   return(lm(formula, data = m4_all_df))
-# }
-# method_err_names <- c(colnames(fcast_smapes_df), colnames(fcast_mases_df))
-# if (use_parallel) {
-#   fits_lm <- mclapply(method_err_names, fit_lm, mc.cores = 16)
-# } else {
-#   fits_lm <- lapply(method_err_names, fit_lm)
-# }
-# names(fits_lm) <- method_err_names
-# print(lapply(fits_lm, summary))
+algo_dates <-
+  c(NA, NA, NA, NA, 1957, 1985, 2000, 1989)
+m4_res <-
+  tibble(YEAR = algo_dates, METHOD = names(mean_errs_df), sMAPE=as.vector(t(mean_errs_df[1,])))
+colnames(m4_res) <- c("YEAR", "METHOD", "sMAPE")
+write_csv(m4_res, "m4_accuracies.csv")
---
+++ benchmark_accuracies_vs_year.R
@@ -0,0 +1,76 @@
+library(lubridate)
+library(tidyverse)
+
+imagenet <- read_csv("ImageNet_accuracies.csv")
+wmt14 <- read_csv("WMT14_EnFr_accuracies.csv")
+m4_top_year <- read_csv("m4_accuracies.csv")
+
+imagenet %>%
+  filter(! is.na(`TOP 5 ACCURACY`)) %>%
+  group_by(YEAR) %>%
+  top_n(1, `TOP 5 ACCURACY`) %>%
+  filter(row_number(`TOP 5 ACCURACY`) == 1) %>%
+  mutate(Accuracy = 100 * `TOP 5 ACCURACY`) %>%
+  select(Accuracy, YEAR, METHOD) %>%
+  arrange(YEAR) ->
+  imagenet_top_year
+
+print(imagenet_top_year)
+
+imagenet_top_year %>%
+  ggplot(aes(x = YEAR, y = Accuracy)) +
+  geom_col(width = 0.5, fill = "blue") +
+  ggtitle("ImageNet Top-5 Accuracies per Year") +
+  ylab("% accuracy") +
+  xlab("Year model was published") +
+  geom_text(
+    aes(y = 50, label = METHOD),
+    colour = "black", size = 5, angle = 90
+  ) +
+  geom_hline(aes(yintercept = 95), colour = "red", linetype = "dashed") +
+  geom_text(aes(y = 97, x = 2012.5), label = "Human level performance") +
+  geom_smooth(method = "lm", colour = "black", se = FALSE) +
+  ylim(0, 100) -> gg
+
+print(gg)
+
+wmt14 %>%
+  filter(! is.na(`BLEU SCORE`)) %>%
+  filter(! is.na(YEAR)) %>%
+  filter(YEAR < 2019) %>%
+  group_by(YEAR) %>%
+  top_n(1, `BLEU SCORE`) %>%
+  select(`BLEU SCORE`, YEAR, METHOD) %>%
+  arrange(YEAR) ->
+  wmt14_top_year
+
+wmt14_top_year %>%
+  ggplot(aes(x = YEAR, y = `BLEU SCORE`)) +
+  geom_col(width = 0.5, fill = "blue") +
+  ggtitle("WMT English to French Machine Translation") +
+  ylab("BLEU Score") +
+  xlab("Year model was published") +
+  geom_text(
+    aes(y = 20, label = METHOD),
+    colour = "black", size = 5, angle = 90
+  ) +
+  geom_smooth(method = "lm", colour = "black", se = FALSE) +
+  ylim(0, 50) -> gg
+
+print(gg)
+
+m4_top_year %>%
+  filter(! is.na(YEAR)) %>%
+  ggplot(aes(x = YEAR, y = sMAPE)) +
+  geom_col(width = 4, fill = "blue") +
+  ggtitle("M4 Competition sMAPE") +
+  ylab("sMAPE") +
+  xlab("Year model was published") +
+  geom_text(
+    aes(y = 7, label = METHOD),
+    colour = "black", size = 5, angle = 90
+  ) +
+  geom_smooth(method = "lm", colour = "black", se = FALSE) +
+  ylim(0, 20) -> gg
+
+print(gg)
