774bad80abc7f13ef52de81e17d5af9355cfd046
Gary Mulder <gjmulder@gmail.com>
2019-11-02 18:54:11 +0200

    Added MNIST, but probably not going to use it

--- benchmark_accuracies_vs_year.R
+++ benchmark_accuracies_vs_year.R
@@ -3,13 +3,14 @@ library(tidyverse)
 
 imagenet <- read_csv("ImageNet_accuracies.csv")
 wmt14 <- read_csv("WMT14_EnFr_accuracies.csv")
+mnist <- read_csv("MNIST_accuracies.csv")
 m4_top_year <- read_csv("m4_accuracies.csv")
 
 imagenet %>%
   filter(! is.na(`TOP 5 ACCURACY`)) %>%
   group_by(YEAR) %>%
   top_n(1, `TOP 5 ACCURACY`) %>%
-  filter(row_number(`TOP 5 ACCURACY`) == 1) %>%
+  filter(row_number(RANK) == 1) %>%
   mutate(Accuracy = 100 * `TOP 5 ACCURACY`) %>%
   select(Accuracy, YEAR, METHOD) %>%
   arrange(YEAR) ->
@@ -20,7 +21,7 @@ print(imagenet_top_year)
 imagenet_top_year %>%
   ggplot(aes(x = YEAR, y = Accuracy)) +
   geom_col(width = 0.5, fill = "blue") +
-  ggtitle("ImageNet Top-5 Accuracies per Year") +
+  ggtitle("ImageNet Top-5 Accuracies") +
   ylab("% accuracy") +
   xlab("Year model was published") +
   geom_text(
@@ -54,15 +55,40 @@ wmt14_top_year %>%
     aes(y = 20, label = METHOD),
     colour = "black", size = 5, angle = 90
   ) +
-  geom_smooth(method = "lm", colour = "black", se = FALSE) +
-  ylim(0, 50) -> gg
+  geom_smooth(method = "lm", colour = "black", se = FALSE) ->
+  gg
 
 print(gg)
+
+# mnist %>%
+#   filter(! is.na(YEAR)) %>%
+#   arrange(desc(`PERCENTAGE ERROR`)) %>%
+#   group_by(YEAR) %>%
+#   top_n(-1, `PERCENTAGE ERROR`) %>%
+#   filter(row_number(RANK) == 1) %>%
+#   select(`PERCENTAGE ERROR`, YEAR, METHOD) %>%
+#   arrange(YEAR) ->
+#   mnist_top_year
+#
+# mnist_top_year %>%
+#   ggplot(aes(x = YEAR, y = `PERCENTAGE ERROR`)) +
+#   geom_col(width = 0.5, fill = "blue") +
+#   ggtitle("MNIST Digit Recognition") +
+#   ylab("% accuracy") +
+#   xlab("Year model was published") +
+#   geom_text(
+#     aes(y = 0.15, label = METHOD),
+#     colour = "black", size = 5, angle = 90
+#   ) +
+#   geom_smooth(method = "lm", colour = "black", se = FALSE) ->
+#   gg
+#
+# print(gg)
 
 m4_top_year %>%
   filter(! is.na(YEAR)) %>%
   ggplot(aes(x = YEAR, y = sMAPE)) +
-  geom_col(width = 4, fill = "blue") +
+  geom_col(width = 2, fill = "blue") +
   ggtitle("M4 Competition sMAPE") +
   ylab("sMAPE") +
   xlab("Year model was published") +
@@ -70,7 +96,6 @@ m4_top_year %>%
     aes(y = 7, label = METHOD),
     colour = "black", size = 5, angle = 90
   ) +
-  geom_smooth(method = "lm", colour = "black", se = FALSE) +
-  ylim(0, 20) -> gg
+  geom_smooth(method = "lm", colour = "black", se = FALSE) -> gg
 
 print(gg)
